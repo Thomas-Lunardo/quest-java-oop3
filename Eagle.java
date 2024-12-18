9f29fdbb43e9dab2f503ac7e5f3ddf864411fdd2
THOMAS <thomaslunardo@gmail.com>
2024-12-18 16:09:03 +0100

    add challenge

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,66 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+        /**
+     * takeOff, must be on ground
+     */
+    @Override
+    public void takeOff() {
+        if (!this.isFlying() && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takesoff in the air.%n", this.getName());
+        }
+    }    
+    
+    /**
+    * fly upward
+    * @param meters altitude increase
+    * @return altitude
+    */
+   @Override
+   public int ascend(int meters) {
+       if (this.flying) {
+           this.altitude = Math.min(this.altitude + meters, 500);
+           System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+       }
+       return this.altitude;
+   }
+
+    /**
+    * glide, must be in the air
+    */
+    @Override
+    public void glide() {
+        if (this.isFlying() && this.altitude > 0) {
+            System.out.printf("glides into the air.");
+        }
+    }    
+    
+    /**
+    * fly downward
+    * @param meters altitude decrease
+    * @return altitude
+    */
+   @Override
+   public int descend(int meters) {
+       if (this.flying) {
+           this.altitude = Math.max(this.altitude - meters, 0);
+           System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+       }
+       return this.altitude;
+   }
+
+    /**
+     * land on the ground, must be on ground
+     */
+    @Override
+    public void land() {
+        if (this.flying && this.altitude <= 1) {
+            System.out.printf("%s land on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't land.%n", this.getName());
+        }
     }
 }
